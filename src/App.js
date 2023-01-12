2359c50067806ca352d7869280367e42fc880c96
HimaGirish314 <Himagirish.bandi@gmail.com>
2023-01-12 22:18:12 +0530

    added functionality to display total cart value

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import Header from './Components/Header/Header';
 import './App.css';
+import {useState} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Cart from './Pages/Cart';
 import Cards from './Components/Cards/Cards';
-import Context from './Components/Context/Context';
-
+// import Context from './Components/Context/Context';
+import { appContext } from './Components/Context/Context';
 function App() {
+  const [busket,setBusket]=useState([])
   return (
     <div>
-      <Context>
+      <appContext.Provider value={{busket,setBusket}}>
         <BrowserRouter>
-          <Header /> 
+          <Header  /> 
           <Route path="/" component={Cards} exact/>
           <Route path="/cart" component={Cart} exact/>
         </BrowserRouter>
-    </Context>
+    </appContext.Provider>
    </div>
   );
 }
